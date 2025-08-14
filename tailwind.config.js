ad539ebc3173b03b43250ef45c15c4b08993d55b
tessarion-tsc7-dot <tessarion.tsc7@gmail.com>
2025-08-14 19:17:22 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages//*.{js,ts,jsx,tsx}",
+    "./components//*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#0A0A0A",
+        silver: "#C0C0C0",
+        teslaRed: "#E82127",
+        lightGray: "#E0E0E0"
+      }
+    }
+  },
+  plugins: []
+};
